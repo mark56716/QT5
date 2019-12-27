4be3fe29f69fc8d1028eebbb03fdf33d0b0aedde
mark56716 <52292115+mark56716@users.noreply.github.com>
2019-12-27 20:46:36 +0400

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,11 +1,11 @@
 #include "QtGuiApplication.h"
 #include <QtWidgets/QApplication>
 
-int main(int argc, char* argv[])
+int main(int argc, char *argv[])
 {
 	QApplication a(argc, argv);
 	QtGuiApplication w;
-	w.setWindowTitle("Encryption and decryption.");
+	w.setWindowTitle("Multiset QT");
 	w.show();
 	return a.exec();
 }
